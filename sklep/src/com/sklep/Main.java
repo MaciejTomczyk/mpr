0c0477c5756398b7779261785c1f962ebafd8b2c
M4sterrr <mrmcmt@gmail.com>
2011-10-18 22:22:27 +0200

    Drobna zmiana w Main

--- sklep/src/com/sklep/Main.java
+++ sklep/src/com/sklep/Main.java
@@ -1,38 +1,33 @@
 package com.sklep;
- 
+
 import java.util.ArrayList;
 import java.util.List;
- 
+
 public class Main {
- 
-        public static void main(String[] args) {
-                System.out.println("+++++++++++++++++++++++++++++++");
- 
-                int f = 0;
-                int i = 0;
-                List<Produkt> produkty = new ArrayList<Produkt>();
-                produkty.add(new Produkt("Johnnie Walker Red ", "60"));
-                f++;
-                produkty.add(new Produkt("Sobieski", "39"));
-                f++;
-                produkty.add(new Produkt("Glenfiddich", "220"));
-                f++;
-                Klient p = new Klient("Maciej", "Tomczyk", produkty);
-                p.printProdukty();
-                for (i = 0; i < f; i++) {
-                        produkty.remove(0);
-                }
- 
- 
-                produkty.add(new Produkt("Bols Blue", "56"));
-                f++;
-                produkty.add(new Produkt("Sheridans", "66"));
-                f++;
-                produkty.add(new Produkt("Johnnie Walker Blue", "400"));
- 
-                Klient g = new Klient("Tadeusz", "Tomczyk", produkty);
-                g.printProdukty();
- 
-        }
-}
- 
+
+	public static void main(String[] args) {
+		System.out.println("+++++++++++++++++++++++++++++++");
+
+		List<Produkt> produkty = new ArrayList<Produkt>();
+		
+	
+		produkty.add(new Produkt("Johnnie Walker Red ", "60"));
+		produkty.add(new Produkt("Sobieski", "39"));
+		produkty.add(new Produkt("Glenfiddich", "220"));
+		Klient p = new Klient("Maciej", "Tomczyk", produkty);
+		p.printProdukty();
+		do {
+			produkty.remove(0);
+
+		} while (produkty.size() != 0);
+
+		produkty.add(new Produkt("Bols Blue", "56"));
+		produkty.add(new Produkt("Sheridans", "66"));
+		produkty.add(new Produkt("Johnnie Walker Blue", "400"));
+		Klient g = new Klient("Tadeusz", "Tomczyk", produkty);
+		g.printProdukty();
+
+	}
+
+		
+	}
